97a3effd676828f07d8bc0034f70d6bff9edc9e1
Leandro Acquati <leandroacquati@gmail.com>
2018-12-23 17:09:33 -0200

    yajra/laravel-datatables-buttons

--- config/app.php
+++ config/app.php
@@ -181,6 +181,7 @@ return [
         \InfyOm\Generator\InfyOmGeneratorServiceProvider::class,
         \InfyOm\AdminLTETemplates\AdminLTETemplatesServiceProvider::class,
         Yajra\DataTables\DataTablesServiceProvider::class,
+        Yajra\DataTables\ButtonsServiceProvider::class,
     ],
 
     /*
